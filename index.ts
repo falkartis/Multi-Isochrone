8c1321ebd57b850e6308a07de25027ae45130efc
Falk <falkartis@gmail.com>
2023-02-18 23:02:04 +0100

    Implemented Place, Destination and DestinationSet.

--- index.ts
+++ index.ts
@@ -18,15 +18,15 @@ function initMap(): void {
 	});
 }
 
-declare global {
-	interface Window {
-		initMap: () => void;
-	}
-}
+// declare global {
+// 	interface Window {
+// 		initMap: () => void;
+// 	}
+// }
 window.initMap = initMap;
 
 
-const form: HTMLFormElement = document.getElementById("markerForm");
+const form = document.getElementById("markerForm") as HTMLFormElement;
 
 form.onsubmit = () => {
 	const formData = new FormData(form);
@@ -38,7 +38,61 @@ form.onsubmit = () => {
 };
 
 
-class Location {
+class Place {
 	Lat: number;
 	Long: number;
+	constructor(lat: number, long: number) {
+		this.Lat = lat;
+		this.Long = long;
+	}
 }
+
+class Destination {
+	Place: Place;
+	Wheight: number;
+	constructor(place: Place, wheight: number) {
+		this.Place = place;
+		this.Wheight = wheight;
+	}
+}
+
+class DestinationSet {
+	Destinations: Destination[];
+	constructor(destinations: Destination[]) {
+		this.Destinations = destinations;
+	}
+	computeCostFrom(origin: Place, fn: (a: Place, b: Place) => number) {
+		let totalCost: number = 0;
+		for (let destination of this.Destinations) {
+			var cost = destination.Wheight * fn(origin, destination.Place);
+			totalCost += cost;
+		}
+		return totalCost;
+	}
+	computeCostFromEuclidean(origin: Place) {
+		return this.computeCostFrom(origin, (a, b) => {
+			var dLat: number = a.Lat - b.Lat;
+			var dLong: number = a.Long - b.Long;
+			return Math.sqrt((dLat * dLat) + (dLong * dLong));
+		});
+	}
+}
+
+// TESTS:
+console.log("Tests:");
+
+
+function testTotalCost() {
+
+
+	var places: Place[] = [new Place(1,0), new Place(0,1), new Place(-1,0), new Place(0,-1)];
+	var destinations: Destination[] = [new Destination(places[0], 1), new Destination(places[1], 2), new Destination(places[2], 3), new Destination(places[3], 4)];
+	var dSet: DestinationSet= new DestinationSet(destinations);
+	var tCost: number = dSet.computeCostFromEuclidean(new Place(0,0));
+	if (tCost == 10) {
+		console.log("10 is OK");
+	} else {
+		console.log("Total cost is: " + tCost + "\n Expected 10.");
+	}
+}
+testTotalCost();
