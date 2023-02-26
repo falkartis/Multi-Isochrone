b26dd3befcf815b044cc7e46313ce3d894847e9d
Falk <falkartis@gmail.com>
2023-02-26 23:20:17 +0100

    Test framework doesn't seem to be working.

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,3 +1,10 @@
+// import { add } from '../src/index';
+
+// describe('testing index file', () => {
+//   test('empty string should result in zero', () => {
+//     expect(add('')).toBe(0);
+//   });
+// });
 
 // TESTS:
 class Tests {
